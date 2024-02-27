49b4bbbf38143449f76b8999819607f4a072a214
ivalimak <ivalimak@student.hive.fi>
2024-02-27 13:53:44 +0200

    Added more input error checks

--- src/input.c
+++ src/input.c
@@ -6,19 +6,21 @@
 /*   By: ivalimak <ivalimak@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 19:00:19 by ivalimak          #+#    #+#             */
-/*   Updated: 2024/02/25 19:57:27 by ivalimak         ###   ########.fr       */
+/*   Updated: 2024/02/27 13:52:37 by ivalimak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
 static size_t	get_arrsize(void *arr);
-static short	*parseline(const char *line, size_t size, size_t lnbr);
+static short	*parseline(const char *line, size_t size, size_t lnbr, char *e);
+static int		convert(short *shortarr, char **strarr, size_t size, char *e);
 
 short	**getsolution(size_t size)
 {
 	size_t	i;
 	short	**out;
+	char	err;
 
 	out = ft_push(ft_alloc(size * sizeof(short *)));
 	if (!out)
@@ -27,16 +29,19 @@ short	**getsolution(size_t size)
 		return (NULL);
 	}
 	i = 0;
+	err = 0;
 	while (i < size)
 	{
 		out[i] = parseline(ft_push(ft_strtrim(get_next_line(0), " \t\n")),
-			size, i);
+			size, i, &err);
 		if (!out[i++])
 		{
 			chk_perror();
 			return (NULL);
 		}
 	}
+	if (err)
+		ft_exit(0);
 	return (out);
 }
 
@@ -52,9 +57,8 @@ static size_t	get_arrsize(void *a)
 	return (i);
 }
 
-static short	*parseline(const char *line, size_t size, size_t lnbr)
+static short	*parseline(const char *line, size_t size, size_t lnbr, char *e)
 {
-	size_t	i;
 	size_t	arrsize;
 	short	*out;
 	char	**arr;
@@ -71,12 +75,29 @@ static short	*parseline(const char *line, size_t size, size_t lnbr)
 		ft_printf("%sKO: too few numbers on line %u: '%s'%s\n",
 			SGR_KO, lnbr + 1, line, SGR_RST);
 	if (arrsize != size)
-		ft_exit(0);
+		*e = 1;
+	if (!convert(out, arr, size, e))
+		ft_printf("%sKO: number out of range on line %u%s\n",
+			SGR_KO, lnbr + 1, SGR_RST);
+	return (out);
+}
+
+
+static int	convert(short *shortarr, char **strarr, size_t size, char *e)
+{
+	size_t	i;
+	int		rv;
+
 	i = 0;
-	while (arr[i] && i < size)
+	rv = 1;
+	while (i < size)
 	{
-		out[i] = ft_atoi(arr[i]);
+		shortarr[i] = ft_atoi(strarr[i]);
+		if (shortarr[i] < 1 || shortarr[i] > (short)size)
+			rv = 0;
 		i++;
 	}
-	return (out);
+	if (!rv)
+		*e = 1;
+	return (rv);
 }
